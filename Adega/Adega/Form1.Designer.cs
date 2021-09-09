cd855f000be563ed19f7dbdb5929cd8f21b1d575
Djavan Camargo <djavanoc@gmail.com>
2021-09-08 22:41:51 -0300

    atualização

---
+++ Adega/Adega/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Adega
+{
+    partial class FormAdega
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.tbDescricao = new System.Windows.Forms.TextBox();
+            this.tbAno = new System.Windows.Forms.TextBox();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.dgAdega = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btInserir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgAdega)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Categoria:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(362, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descrição";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(557, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ano:";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(68, 35);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(100, 20);
+            this.tbNome.TabIndex = 4;
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(256, 35);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(100, 20);
+            this.tbCategoria.TabIndex = 5;
+            // 
+            // tbDescricao
+            // 
+            this.tbDescricao.Location = new System.Drawing.Point(434, 35);
+            this.tbDescricao.Name = "tbDescricao";
+            this.tbDescricao.Size = new System.Drawing.Size(100, 20);
+            this.tbDescricao.TabIndex = 6;
+            // 
+            // tbAno
+            // 
+            this.tbAno.Location = new System.Drawing.Point(592, 35);
+            this.tbAno.Name = "tbAno";
+            this.tbAno.Size = new System.Drawing.Size(100, 20);
+            this.tbAno.TabIndex = 7;
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(272, 71);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpar.TabIndex = 8;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // dgAdega
+            // 
+            this.dgAdega.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgAdega.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colNome,
+            this.colCategoria,
+            this.colDescricao,
+            this.colAno});
+            this.dgAdega.Location = new System.Drawing.Point(10, 113);
+            this.dgAdega.Name = "dgAdega";
+            this.dgAdega.Size = new System.Drawing.Size(778, 325);
+            this.dgAdega.TabIndex = 9;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.Name = "colID";
+            this.colID.Width = 40;
+            // 
+            // colNome
+            // 
+            this.colNome.HeaderText = "Nome";
+            this.colNome.Name = "colNome";
+            this.colNome.Width = 150;
+            // 
+            // colCategoria
+            // 
+            this.colCategoria.HeaderText = "Categoria";
+            this.colCategoria.Name = "colCategoria";
+            this.colCategoria.Width = 150;
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.HeaderText = "Descrição";
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.Width = 300;
+            // 
+            // colAno
+            // 
+            this.colAno.HeaderText = "Ano";
+            this.colAno.Name = "colAno";
+            this.colAno.Width = 95;
+            // 
+            // btInserir
+            // 
+            this.btInserir.Location = new System.Drawing.Point(446, 71);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.TabIndex = 10;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.UseVisualStyleBackColor = true;
+            this.btInserir.Click += new System.EventHandler(this.btInserir_Click);
+            // 
+            // FormAdega
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btInserir);
+            this.Controls.Add(this.dgAdega);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.tbAno);
+            this.Controls.Add(this.tbDescricao);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAdega";
+            this.Text = "Adega";
+            this.Load += new System.EventHandler(this.FormAdega_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgAdega)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbCategoria;
+        private System.Windows.Forms.TextBox tbDescricao;
+        private System.Windows.Forms.TextBox tbAno;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.DataGridView dgAdega;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAno;
+        private System.Windows.Forms.Button btInserir;
+    }
+}
+
